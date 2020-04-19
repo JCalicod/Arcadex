afae1ff7aa9f6ef1ec58c831e22a130db5116580
JCalicod <jcalicod@gmail.com>
2020-04-19 16:16:28 +0200

    Création d'un component PokemonCard

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,16 +8,7 @@
       <div class="pokemons-list">
         <div class="pokemons-list-not-empty" v-if="pokemons.length > 0">
           <div class="list-col" v-for="pokemon in pokemons" :key="pokemon.id">
-              <div class="pokemon-card">
-                <div class="square">
-                  <router-link :to="{ path: 'Pokemon/' + pokemon.id }">
-                    <img :src="'https://www.pokebip.com/pokedex-images/artworks/' + pokemon.id + '.png'" :alt="pokemon.names[6].name">
-                  </router-link>
-                </div>
-                <div class="text-content">
-                  #{{ pokemon.id }} - {{ pokemon.names[6].name }}
-                </div>
-              </div>
+              <PokemonCard :pokemon="pokemon"/>
           </div>
         </div>
         <div class="empty-list" v-else>
@@ -40,10 +31,13 @@
 </template>
 
 <script>
+import PokemonCard from './PokemonCard.vue';
 
 export default {
   name: 'Home',
-
+  components: {
+    PokemonCard
+  },
   data: () => {
     return {
       allPokemons: [],
@@ -205,35 +199,6 @@ export default {
   .list-col {
     flex: 1 0 calc(30% - 10px);
     padding: 10px;
-  }
-
-  .pokemon-card {
-    height: 200px;
-    margin-bottom: 20px;
-  }
-
-  .pokemon-card .square {
-    background-color: #3F4A4F;
-    width: 150px;
-    height: 150px;
-    padding: 10px;
-    border-radius: 5px;
-    margin: auto;
-  }
-
-  .pokemon-card .square:hover {
-    cursor: pointer;
-    background-color: #CF3935;
-  }
-
-  .pokemon-card .text-content {
-    text-align: center;
-    margin-top: 5px;
-    color: #3F4A4F;
-  }
-
-  .pokemon-card .square img {
-    width: 100%;
   }
   
 </style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="pokemon-card">
+        <div class="square">
+            <router-link :to="{ path: 'Pokemon/' + pokemon.id }">
+                <img :src="'https://www.pokebip.com/pokedex-images/artworks/' + pokemon.id + '.png'" :alt="pokemon.names[6].name">
+            </router-link>
+        </div>
+        <div class="text-content">
+            #{{ pokemon.id }} - {{ pokemon.names[6].name }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PokemonCard', 
+  props: {
+      pokemon: Object
+  }
+}
+
+</script>
+
+<style scoped>
+    .pokemon-card {
+    height: 200px;
+    margin-bottom: 20px;
+  }
+
+  .pokemon-card .square {
+    background-color: #3F4A4F;
+    width: 150px;
+    height: 150px;
+    padding: 10px;
+    border-radius: 5px;
+    margin: auto;
+  }
+
+  .pokemon-card .square:hover {
+    cursor: pointer;
+    background-color: #CF3935;
+  }
+
+  .pokemon-card .text-content {
+    text-align: center;
+    margin-top: 5px;
+    color: #3F4A4F;
+  }
+
+  .pokemon-card .square img {
+    width: 100%;
+  }
+</style>
